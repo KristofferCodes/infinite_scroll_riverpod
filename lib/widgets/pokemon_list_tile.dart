ed680957c8c0f293a63593173aa215337d640d39
mac <kristofferjaymes@gmail.com>
2024-05-30 11:16:08 +0100

    added skelotonizer

--- lib/widgets/pokemon_list_tile.dart
+++ lib/widgets/pokemon_list_tile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:infinite_scroll_riverpod/providers/pokemon_data_providers.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import '../models/pokemon.dart';
 
 class PokemonListTile extends ConsumerWidget {
   final String pokemonURL;
@@ -10,17 +13,28 @@ class PokemonListTile extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final pokemon = ref.watch(pokemonDataProvider(pokemonURL));
     return pokemon.when(data: (data) {
-      return _tile(context);
+      return _tile(context, false, data);
     }, error: (error, st) {
       return Text('Error: $error');
     }, loading: () {
-      return _tile(context);
+      return _tile(context, true, null);
     });
   }
 
-  Widget _tile(BuildContext context) {
-    return ListTile(
-      title: Text(pokemonURL),
+  Widget _tile(BuildContext context, bool isLoading, Pokemon? pokemon) {
+    return Skeletonizer(
+      enabled: isLoading,
+      child: ListTile(
+        leading: pokemon != null
+            ? CircleAvatar(
+                backgroundImage: NetworkImage(pokemon.sprites!.frontDefault!),
+              )
+            : CircleAvatar(),
+        title: Text(pokemon != null
+            ? pokemon.name!.toUpperCase()
+            : "currently loading name for pokemon"),
+        subtitle: Text("Has ${pokemon?.moves?.length.toString() ?? 0} moves"),
+      ),
     );
   }
 }
