9eec4370dcd67bf9a20efcee3ac476fde7032278
mac <kristofferjaymes@gmail.com>
2024-05-30 10:26:56 +0100

    register endpoints

--- lib/controllers/home_page_controller.dart
+++ lib/controllers/home_page_controller.dart
@@ -1,7 +1,34 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
+import 'package:infinite_scroll_riverpod/models/pokemon.dart';
 
 import '../models/page_date.dart';
+import '../services/http_service.dart';
 
-class HomePageController extends StateNotifier<HomePageData>{
-  HomePageController(super._state);
+class HomePageController extends StateNotifier<HomePageData> {
+  final GetIt _getIt = GetIt.instance;
+
+  late HTTPService _httpService;
+
+  HomePageController(super._state) {
+    _httpService = _getIt.get<HTTPService>();
+    _setup();
+  }
+
+  Future<void> _setup() async {
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    if (state.data == null) {
+      Response? res = await _httpService
+          .get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+      if(res != null && res.data != null){
+        PokemonListData data = PokemonListData.fromJson(res.data);
+        state = state.copyWith(data: data);
+      }
+      print(res?.data);
+    } else {}
+  }
 }
